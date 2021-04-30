0d7b29a4945916bab0a9cce5dca5bdd418786750
AlexBuratta13 <alex.buratta13@gmail.com>
2021-04-30 08:52:25 +0200

    Aggiunta async e await

--- Wpf_Task/MainWindow.xaml.cs
+++ Wpf_Task/MainWindow.xaml.cs
@@ -25,27 +25,58 @@ namespace Wpf_Task
         {
             InitializeComponent();
         }
-        public static int TrovaMultipli(int a)
+        public async Task TrovaMultipli(int a)
         {
-            int multipli = 0;
-            for (int i = 0; i < 200000000; i++)
+            await Task.Run(() =>
             {
-                if (i % a == 0)
+                int divisori = 200000000;
+                int multipli = 0;
+                for (int i = 0; i < divisori; i++)
                 {
-                    multipli++;
+                    if ((i % a) == 0)
+                    {
+                        multipli++;
+                    }
+                    if (i % 200000000 == 0)
+                    {
+                        pb1.Dispatcher.Invoke(() =>
+                        pb1.Value++);
+                    };
                 }
-            }
-            return multipli;
+
+                lbl_Risultato.Dispatcher.Invoke(() =>
+                    lbl_Risultato.Content = multipli
+                );
+            });
         }
             private void btn_esegui_Click(object sender, RoutedEventArgs e)
         {
             int a = int.Parse(txt_inserisci.Text);
-            Task<int> task1 = Task.Factory.StartNew(() => TrovaMultipli(a),
-                CancellationToken.None,
-                TaskCreationOptions.LongRunning,
-                TaskScheduler.Default
-                );
-            lbl_Risultato.Content = $" Risultato: {task1.Result}";
+            pb1.Minimum = 0;
+            pb1.Maximum = 100;
+            pb1.Value = 0;
+            lbl_Risultato.Content = $" Risultato: {TrovaMultipli(a)}";
+        }
+
+        private void btn_primo_Click(object sender, RoutedEventArgs e)
+        {
+            bool primo = true;
+            int a = int.Parse(txt_inserisci.Text);
+            for (int i = 2; i <= a / 2; i++)
+            {
+                if (a % i == 0)
+                {
+                    primo = false;
+                }
+            }
+            if (primo == false)
+            {
+                lbl_Ris_Primo.Content = "Il numero non è primo";
+            }
+            else
+            {
+                lbl_Ris_Primo.Content = "Il numero è primo";
+            }
         }
     }
 }
